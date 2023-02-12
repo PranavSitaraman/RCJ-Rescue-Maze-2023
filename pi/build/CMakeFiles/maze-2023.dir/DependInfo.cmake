9bc67b3ab3e5a43afaa1d2fde94b91e6bb1604b1
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-02-12 17:06:31 -0500

    update

---
+++ pi/build/CMakeFiles/maze-2023.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/Serial.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/Serial.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/SparkFun_AS726X_Arduino_Library/src/AS726X.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/SparkFun_AS726X_Arduino_Library/src/AS726X.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/Wire.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/Wire.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/camera.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/camera.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/main.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/main.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/matrix.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/matrix.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/search.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/search.cpp.o"
+  "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/tile.cpp" "/mnt/c/Users/Pranav Sitaraman/Documents/Programming/Arduino/RCJ Rescue Maze 2023/pi/build/CMakeFiles/maze-2023.dir/tile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARDUINO=100"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SparkFun_AS726X_Arduino_Library/src"
+  "../arduino-compat"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pi/build/CMakeFiles/maze-2023.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maze-2023.dir/Serial.cpp.o"
+  "CMakeFiles/maze-2023.dir/SparkFun_AS726X_Arduino_Library/src/AS726X.cpp.o"
+  "CMakeFiles/maze-2023.dir/Wire.cpp.o"
+  "CMakeFiles/maze-2023.dir/camera.cpp.o"
+  "CMakeFiles/maze-2023.dir/main.cpp.o"
+  "CMakeFiles/maze-2023.dir/matrix.cpp.o"
+  "CMakeFiles/maze-2023.dir/search.cpp.o"
+  "CMakeFiles/maze-2023.dir/tile.cpp.o"
+  "maze-2023"
+  "maze-2023.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maze-2023.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
