0bd723d322f6d96368e14d911b24f0c8b7f40744
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-02-18 22:19:00 -0500

    Update camera.hpp

--- pi/camera.hpp
+++ pi/camera.hpp
@@ -5,6 +5,10 @@
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/ml.hpp>
 #include "search.hpp"
 namespace Letter
 {
