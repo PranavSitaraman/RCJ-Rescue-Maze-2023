11c4f1eb67f67b0ef4f078d0355eaa8c22888ee9
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-02-19 17:17:03 -0500

    Update search.cpp

--- pi/search.cpp
+++ pi/search.cpp
@@ -44,6 +44,7 @@ Search::Search(Serial &ser, std::mutex &lock, std::condition_variable &cv, const
     constexpr std::uint8_t RESET = Dir::W + 1;
 #ifndef VIRTUAL_TEST
     serial.write((std::uint8_t)((1 << 7) | RESET));
+    while (serial.available() == 0);
     uint8_t a = serial.read();
     std::cout << a << std::endl;
 #endif
