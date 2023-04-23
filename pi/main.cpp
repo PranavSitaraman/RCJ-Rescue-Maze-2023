68c0d2dae6aea66ea50fc0b0c663daa951ec7181
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-04-23 15:56:32 -0400

    Update main.cpp

--- pi/main.cpp
+++ pi/main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
 #ifndef VIRTUAL_TEST
-    std::atomic<ThreadState> thread_state = ThreadState::INIT;
+    //std::atomic<ThreadState> thread_state = ThreadState::INIT;
 #endif
     std::mutex map_lock;
     std::condition_variable map_cv;
@@ -43,14 +43,14 @@ int main(int argc, char **argv)
 #endif
     Search *search = &searches[current];
 #ifndef VIRTUAL_TEST
-    std::thread camera_thread(&detect, std::ref(thread_state), &search, std::ref(map_lock), std::ref(map_cv));
+    //std::thread camera_thread(&detect, std::ref(thread_state), &search, std::ref(map_lock), std::ref(map_cv));
 #endif
     std::stack<std::uint8_t> path;
     search->check_walls();
     search->print_map();
 #ifndef VIRTUAL_TEST
-    while (thread_state == ThreadState::INIT)
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //while (thread_state == ThreadState::INIT)
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(1));
 #endif
 restart:
     while (!(path = search->search()).empty())
@@ -116,7 +116,7 @@ restart:
     search->move(search->search());
     search->print_map();
 #ifndef VIRTUAL_TEST
-    thread_state = ThreadState::STOP;
-    camera_thread.join();
+    //thread_state = ThreadState::STOP;
+    //camera_thread.join();
 #endif
 }
