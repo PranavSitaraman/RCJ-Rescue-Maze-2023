8cb734a6daaa8acb23afd99b584c57fc628129b4
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-05-14 16:59:44 -0400

    Update camera.cpp

--- pi/camera.cpp
+++ pi/camera.cpp
@@ -50,7 +50,8 @@ void detect(std::atomic<ThreadState> &state, Search **search, std::mutex &map_lo
         for (std::uint8_t i = 0; i < caps.size() && state != ThreadState::STOP; i++)
         {
             caps[i] >> frame;
-            cv::imshow("froriginal", frame);
+            if (i == 0) cv::imshow("froriginal0", frame);
+            else cv::imshow("froriginal1", frame);
             std::uint8_t n_kits = 0;
             bool vic = false;
             switch (color_detect(frame))
@@ -136,10 +137,8 @@ Letter::letter letter_detect(cv::Mat &frame)
     std::array<int, 3> letterCount{};
     cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
     //cv::convertScaleAbs(frame, frame, 1.5, 4);
-    cv::medianBlur(frame, frame, 1);
     cv::GaussianBlur(frame, frame, cv::Size(5, 5), 0);
-cv::threshold(frame, frame, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
-		cv::Canny(frame, frame, 50, 110);
+		cv::adaptiveThreshold(frame, frame, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
         std::vector<std::vector<cv::Point>> contours;
     cv::findContours(frame, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
     cv::drawContours(frame, contours, -1, cv::Scalar(255, 255, 255), -1);
