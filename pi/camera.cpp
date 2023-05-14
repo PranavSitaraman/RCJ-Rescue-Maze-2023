35a9207ff776d0d10dd606d7d03426a58009c0a8
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-05-14 16:32:38 -0400

    Update camera.cpp

--- pi/camera.cpp
+++ pi/camera.cpp
@@ -131,7 +131,7 @@ Letter::letter letter_detect(cv::Mat &frame)
     std::array<int, 3> letterCount{};
     cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
     cv::GaussianBlur(frame, frame, cv::Size(5, 5), 0);
-    cv::threshold(frame, frame, 80, 255, cv::THRESH_BINARY_INV);
+    cv::threshold(frame, frame, 100, 255, cv::THRESH_BINARY_INV);
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(frame, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
     cv::drawContours(frame, contours, -1, cv::Scalar(255, 255, 255), -1);
