c8d56d01c35821a804bd46319e9e51cb54fcd427
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-05-21 10:58:47 -0400

    Update camera.cpp

--- pi/camera.cpp
+++ pi/camera.cpp
@@ -15,8 +15,8 @@
 #include "search.hpp"
 #include "Serial.hpp"
 namespace fs = std::filesystem;
-constexpr auto SIZE_THRESH = 50;
-constexpr auto SLICE_SIZE_THRESH = 100;
+constexpr auto SIZE_THRESH = 60;
+constexpr auto SLICE_SIZE_THRESH = 120;
 void detect(std::atomic<ThreadState> &state, Search **search, std::mutex &map_lock, std::condition_variable &map_cv)
 {
     std::string port;
@@ -74,10 +74,10 @@ void detect(std::atomic<ThreadState> &state, Search **search, std::mutex &map_lo
                 n_kits = 3;
                 std::cout << "H" << std::endl;
                 break;
-            // case Letter::S:
-            //    n_kits = 2;
-            //    std::cout << "S" << std::endl;
-            //    break;
+            case Letter::S:
+                n_kits = 2;
+                std::cout << "S" << std::endl;
+                break;
             case Letter::U:
                 vic = true;
                 std::cout << "U" << std::endl;
@@ -148,7 +148,7 @@ Letter::letter letter_detect(cv::Mat &frame)
         auto angle = minRect.angle;
         if (rw < SIZE_THRESH || rh < SIZE_THRESH)
             continue;
-        if (rw / rh > 2 || rh / rw > 2)
+        if ((double)rw / rh > 1.7 || (double)rh / rw > 1.7)
             continue;
         cv::Mat letter = 255 - frame(cv::boundingRect(contour)).clone();
         int mxsz = std::max(letter.cols, letter.rows);
