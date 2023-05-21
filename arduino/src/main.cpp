1560bf63de630504027718d9c4dd3a0873cee227
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2023-05-20 22:17:03 -0400

    Update main.cpp

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -238,7 +238,7 @@ uint8_t move(const bool dir[2], double a, double motorSpeed)
       colors = tiles(0);
     else
       colors = tiles(1);
-    if (colors.R < 500 && colors.G < 500 && colors.B < 500)
+    if (colors.R < 300 && colors.G < 300 && colors.B < 300)
     {
       uint16_t reverse = encoder;
       motorReset();
